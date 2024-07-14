77857273d6ccce0403991d475512fd71adfa5f16
Red Williams <redpwilliams@gmail.com>
2024-07-14 02:41:31 -0500

    feat: update sprite light on animation

--- Assets/Scripts/UpdateSpriteLight.cs
+++ Assets/Scripts/UpdateSpriteLight.cs
@@ -5,21 +5,23 @@ using UnityEngine.Rendering.Universal;
 public class UpdateSpriteLight : MonoBehaviour
 {
    private SpriteRenderer _sr;
+   private Sprite _sprite;
    private Light2D _light;
 
-   public void Start()
+   private void Start()
    {
       _sr = gameObject.GetComponent<SpriteRenderer>();
       _light = gameObject.GetComponent<Light2D>();
    }
 
+   private void LateUpdate()
+   {
+      _sprite = _sr.sprite;
+   }
+
    public void UpdateLight2D()
    {
-      Sprite sprite = _sr.sprite;
-
-      if (_light != null)
-      {
-         // _light.lightCookieSprite = sprite;
-      }
+         _light.lightCookieSprite = _sprite;
+         Debug.Log(_sprite);
    } 
 }
