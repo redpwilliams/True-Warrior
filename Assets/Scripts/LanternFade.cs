578e21fee74bb3e004b38e49d8e1574724159673
Red Williams <redpwilliams@gmail.com>
2024-07-16 15:40:51 -0500

    scene: brighten moonlights, apply light pulse

--- Assets/Scripts/LanternFade.cs
+++ Assets/Scripts/LanternFade.cs
@@ -6,6 +6,8 @@ public class LanternFade : MonoBehaviour
 {
     private Light2D _light;
     [SerializeField] private float _oscillationFrequency = 1f;
+    [SerializeField] private float _minIntensity = 0.8f;
+    [SerializeField] private float _maxIntensity = 1.5f;
 
     private void Start()
     {
@@ -14,7 +16,7 @@ public class LanternFade : MonoBehaviour
     
     private void Update()
     {
-        _light.intensity = Mathf.Lerp(0.8f, 1.5f, Mathf
+        _light.intensity = Mathf.Lerp(_minIntensity, _maxIntensity, Mathf
         .PingPong(Time.time * _oscillationFrequency, 1f));
     }
 }
