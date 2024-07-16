ca8f213ec3ec3e4e0233782ce88a35cdf35221fe
Red Williams <redpwilliams@gmail.com>
2024-07-16 12:25:58 -0500

    refactor: individually select time between stages

--- Assets/Scripts/HaikuText.cs
+++ Assets/Scripts/HaikuText.cs
@@ -10,6 +10,12 @@ public class HaikuText : MonoBehaviour
 {
     private TextMeshProUGUI _tmp;
 
+    [SerializeField] private float timeUntilStage1= 2.5f;
+    [SerializeField] private float timeUntilStage2 = 5f;
+    [SerializeField] private float timeUntilStage3 = 5f;
+    // TODO - Make range?
+    [SerializeField] private float timeUntilBattleStart = 5f;
+
     private void Awake()
     {
         _tmp = GetComponent<TextMeshProUGUI>();
@@ -27,13 +33,26 @@ public class HaikuText : MonoBehaviour
         // Initial startup buffer
         yield return new WaitForSeconds(2.5f);
         
-        // Display lines
+        // Choose haiku
         JsonReader.Haiku haiku = haikus[Random.Range(0, haikus.Count)];
-        for (int i = 0; i < 3; i++)
-        {
-            _tmp.text = haiku.lines[i];
-            yield return new WaitForSeconds(5);
-        }
+        
+        int stage = 0;
+
+        // Line 1
+        yield return new WaitForSeconds(timeUntilStage1);
+        _tmp.text = haiku.lines[stage++];
+        
+        // Line 2
+        yield return new WaitForSeconds(timeUntilStage2);
+        _tmp.text = haiku.lines[stage++];
+        
+        // Line 3
+        yield return new WaitForSeconds(timeUntilStage3);
+        _tmp.text = haiku.lines[stage];
+        
+        // Battle Start
+        yield return new WaitForSeconds(timeUntilBattleStart);
+        // TODO - Change text to different formatted text object?
     }
 
 
