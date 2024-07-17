3fe0c80cdc6f70eb4ac44a019cdb8c6d444762f5
Red Williams <redpwilliams@gmail.com>
2024-07-17 03:00:36 -0500

    feat: begin implementing player controls

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Character))]
+public class Movement : MonoBehaviour
+{
+    private Character _character;
+
+    private void Awake()
+    {
+        _character = GetComponent<Character>();
+    }
+
+    private void Start()
+    {
+        Debug.Log("Got character: " + _character);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
