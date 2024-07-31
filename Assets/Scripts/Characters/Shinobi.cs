3a4f3159c3ec7e732cab2c0518cf53d8a38e1549
Red Williams <redpwilliams@gmail.com>
2024-07-31 13:52:27 -0400

    refactor: start changing shinobi attack
    instead of a jump, fall, then strike sequence, I'll lower the opacity of the character, move it behind the opponent, then turn around and strike it

--- Assets/Scripts/Characters/Shinobi.cs
+++ Assets/Scripts/Characters/Shinobi.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 namespace Characters
 {
@@ -15,11 +17,16 @@ namespace Characters
         [SerializeField] private float _jumpForwardForce = 80f;
         private static readonly int Slashing = Animator.StringToHash("ShouldSlash");
 
+        private SpriteRenderer _sr;
+        private Light2D _light;
+
         
         protected override void Start()
         {
             base.Start();
             Rb2d.gravityScale = _risingGravityScale;
+            _sr = GetComponent<SpriteRenderer>();
+            _light = GetComponent<Light2D>();
         }
 
         private void Update()
@@ -28,6 +35,7 @@ namespace Characters
             Anim.SetTrigger(Falling);
             Rb2d.gravityScale = _fallingGravityScale;
         }
+        
         protected override string CharacterTitle() => "Shinobi/忍び";
 
         #region Animation Events
@@ -37,9 +45,49 @@ namespace Characters
         {
             Rb2d.velocity = Vector2.zero;
 
-            int direction = _playerType == PlayerType.One ? 1 : -1;
-            Rb2d.AddForce(new Vector2(_jumpForwardForce * direction, 
-            _jumpUpwardForce), ForceMode2D.Impulse);
+            // int direction = _playerType == PlayerType.One ? 1 : -1;
+            // Rb2d.AddForce(new Vector2(_jumpForwardForce * direction, 
+            // _jumpUpwardForce), ForceMode2D.Impulse);
+
+            StartCoroutine(SmokeBall(true));
+            
+            IEnumerator SmokeBall(bool shouldDisappear)
+            {
+                // Sprite alpha
+                float startAlpha = shouldDisappear ? 1 : 0;
+                float endAlpha = shouldDisappear ? 0 : 1;
+                Color sc = _sr.material.color;
+                
+                // Light2D intensity
+                float initialIntensity = _light.intensity;
+                float targetFadeIntensity = 0.3f;
+                float startIntensity = shouldDisappear ? initialIntensity : targetFadeIntensity;
+                float endIntensity = shouldDisappear ? targetFadeIntensity : initialIntensity;
+
+                float animationDuration = 0.5f;
+                float elapsedTime = 0f;
+                
+                while (elapsedTime < animationDuration)
+                {
+                    float t = elapsedTime / animationDuration;
+                    
+                    // Animate sprite alpha
+                    _sr.material.color =
+                        new Color(sc.r, sc.g, sc.b, Mathf.Lerp(startAlpha, 
+                        endAlpha, Lerp2D.EaseOutExpo(t)));
+                    
+                    // Animate sprite light intensity
+                    _light.intensity = Mathf.Lerp(startIntensity,
+                        endIntensity, Lerp2D.EaseOutExpo(t));
+
+                    elapsedTime += Time.deltaTime;
+                    yield return null;
+                }
+
+                // Snap to final values after animation
+                _sr.material.color = new Color(sc.r, sc.g, sc.b, endAlpha);
+                _light.intensity = endIntensity;
+            } 
         }
 
         [UsedImplicitly]
