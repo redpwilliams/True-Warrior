cd83fe021807631961d72374c643db0ade7d1d28
Red Williams <redpwilliams@gmail.com>
2024-08-01 21:17:57 -0400

    refactor: declassify button manager as singleton

--- Assets/Scripts/Managers/ButtonManager.cs
+++ Assets/Scripts/Managers/ButtonManager.cs
@@ -4,19 +4,6 @@ namespace Managers
 {
     public class ButtonManager : MonoBehaviour
     {
-        /// Singleton Instance
-        public static ButtonManager Buttons { get; private set; }
 
-        private void Awake()
-        {
-            if (Buttons != null && Buttons != this)
-            {
-                Destroy(Buttons);
-                return;
-            }
-
-            Buttons = this;
-            DontDestroyOnLoad(gameObject);
-        }
     }
 }
