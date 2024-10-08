0bcbf7c8876a9758be3b85617608c4205e5f6aa6
Red Williams <redpwilliams@gmail.com>
2024-10-08 19:34:50 -0400

    working: start adding support for saving player pref character

--- Assets/Editor/MenuItems.cs
+++ Assets/Editor/MenuItems.cs
@@ -1,8 +1,10 @@
+using System;
 using Managers;
 using UnityEditor;
 using Characters;
 using JetBrains.Annotations;
 using UI;
+using UI.Buttons;
 using UnityEngine;
 
 public class MenuItems : Editor
@@ -19,8 +21,26 @@ public class MenuItems : Editor
     private const string SaveActionsSubMenu =
         DeveloperActionsMenu + "Save\\Load Actions/";
 
-    private const string PrintSavedSamuraiType =
-        SaveActionsSubMenu + "Print Saved Samurai Type";
+    private const string PrintSavedCharacter =
+        SaveActionsSubMenu + "Print Saved Character";
+
+    private const string SetSavedCharacter =
+        SaveActionsSubMenu + "Set Saved Character/";
+
+
+    [SerializeField] private int _test;
+    // private static MenuButton _optionsButton;
+    // private static OptionsMenuButton _roninButton;
+    // private static OptionsMenuButton _shinobiButton;
+    // private static OptionsMenuButton _shogunButton;
+    private const string Ronin = SetSavedCharacter + "Ronin";
+    private const string Shinobi  = SetSavedCharacter + "Shinobi ";
+    private const string Shogun = SetSavedCharacter + "Shogun";
+
+    private void OnEnable()
+    {
+        Debug.Log("Test");
+    }
 
     #region BeginAttack
 
@@ -48,12 +68,34 @@ public class MenuItems : Editor
 
     #region Save/Load
 
-    [MenuItem(PrintSavedSamuraiType)]
+    [MenuItem(PrintSavedCharacter)]
     private static void PrintSavedSamurai()
     {
         SamuraiType st = SaveManager.LoadPlayerCharacter();
         Debug.Log(st);
     }
 
+    [MenuItem(Ronin)]
+    private static void SetRonin()
+    {
+        EventManager.Events.DeselectAllChosenTOs();
+        SaveManager.SavePlayerCharacter(SamuraiType.Ronin);
+    }
+
+    [MenuItem(Shinobi)]
+    private static void SetShinobi()
+    {
+        EventManager.Events.DeselectAllChosenTOs();
+        SaveManager.SavePlayerCharacter(SamuraiType.Shinobi);
+    }
+
+    [MenuItem(Shogun)]
+    private static void SetShogun(MenuCommand mc)
+    {
+        EventManager.Events.DeselectAllChosenTOs();
+        SaveManager.SavePlayerCharacter(SamuraiType.Shogun);
+        // _optionsButton.SetFirstButton();
+    }
+    
     #endregion
 }
