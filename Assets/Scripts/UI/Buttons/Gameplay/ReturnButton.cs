71b556e0a71bcd5857d067a6943757aa8b932277
Red Williams <redpwilliams@gmail.com>
2024-11-13 23:11:53 -0500

    chore: rename quit button to return

--- Assets/Scripts/UI/Buttons/Gameplay/ReturnButton.cs
+++ Assets/Scripts/UI/Buttons/Gameplay/ReturnButton.cs
@@ -2,7 +2,7 @@ using UnityEngine.EventSystems;
 
 namespace UI.Buttons.Gameplay
 {
-    public class QuitModeButton : GameplayButton
+    public class ReturnButton : GameplayButton
     {
         public override void OnSubmit(BaseEventData eventData)
         {
